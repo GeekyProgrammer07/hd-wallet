9abf51e3b2cca683d66b2f74468a7957e8896c3e
GeekyProgrammer07 <sumansamantaiitkgp@gmail.com>
2025-04-04 22:33:17 +0000

    Update backend.js
    Add /ethKeys route for generating Ethereum accounts

--- backend/backend.js
+++ backend/backend.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const bip = require("bip39");
+const ethWallet = require("ethereumjs-wallet");
+const hdkey = ethWallet.hdkey;
+
+var ethAccCounter = 0;
 
 const app = express()
 app.use(express.json());
@@ -9,6 +13,18 @@ app.get("/seedphrase", (req, res) => {
     console.log(mnemonics);
     const seed = bip.mnemonicToSeedSync(mnemonics);
     res.send(mnemonics);
+})
+
+app.post("/ethKeys", (req, res) => {
+    const seed = Buffer.from(req.body.seed, 'hex');
+    const hdwallet = hdkey.fromMasterSeed(seed); //An HD wallet root node, which contains: 128-bit chain code 256-bit private key
+    const path = `m/44'/60'/${ethAccCounter}'/0/0`; // Ethereum derivation path
+    const hdWalletNode = hdwallet.derivePath(path);
+    const account = hdWalletNode.getWallet().getAddressString();
+    ethAccCounter++; 
+    res.json({
+        publicKey: account
+    })   
 })
 
 app.listen(3000);
